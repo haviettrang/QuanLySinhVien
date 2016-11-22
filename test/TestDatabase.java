5a6906a6ea703a4c9d0dfe419a7d14aa028b8c95
haviettrang <trang12596@gmail.com>
2016-11-22 21:08:58 +0700

    create test file

---
+++ test/TestDatabase.java
@@ -0,0 +1,17 @@
+
+import database.ConnectDatabase;
+
+/**
+ * @author Hà Viết Tráng - HAVIETTRANG
+ * @date Nov 18, 2016 10:47:22 PM
+ * @website haviettrang.blogspot.com
+ * @Notes View my notes at haviettrang.postach.io
+ */
+public class TestDatabase {
+    public static void main(String[] args) {
+        ConnectDatabase cd = new ConnectDatabase();
+        cd.connectToDatabase();
+        System.out.println(cd.getConnection());
+        cd.disconnectToDatabase();
+    }
+}
